c789d4517f52c455cbb7efd7487c4772ef609376
Ayondip Jana <64276906+dazal-32@users.noreply.github.com>
2021-02-19 13:01:33 +0530

    Update connection.php

--- assets/require/connection.php
+++ assets/require/connection.php
@@ -1,19 +1,5 @@
 <?php
 session_start();
-$con= mysqli_connect('localhost','root','');
-mysqli_select_db($con,'nandi_studio');
-/*
-$con= mysqli_connect('localhost','u540231764_nandi','G:wcS704');
-mysqli_select_db($con,'u540231764_nandi_studio');*/
-/*if(!isset($_SESSION['USER_ID']))
-{
-	$_SESSION['USER_ID']='0';
-	}*/
-/*if($con)
-{
-echo"conected";
-}
-else{
-echo"not conected";
-}*/
+$con= mysqli_connect('host','host_name','host_password');
+mysqli_select_db($con,'db_name');
 ?>
