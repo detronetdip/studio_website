534eaf927fbfc1116067b7000bf79b334cf31c00
Ayondip Jana <64276906+dazal-32@users.noreply.github.com>
2021-02-19 13:07:58 +0530

    Update sentreply.php

--- Admin/assets/Allajaxphp/contact/sentreply.php
+++ Admin/assets/Allajaxphp/contact/sentreply.php
@@ -3,7 +3,7 @@ require('../../require/connection.php');
 require('../../require/function.php');
 $mail=$_POST['sender_mail'];
 $msg=$_POST['msg'];
-$email="nandistudio.anandapur87@gmail.com";
+$email="****@gmail.com";
 $to=$mail;
 $subject='REPLY';
 $message=$msg;
