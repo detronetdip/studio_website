9970763adba071938520f2620204580a08ec0fd4
Ayondip Jana <64276906+dazal-32@users.noreply.github.com>
2021-02-19 13:04:48 +0530

    Update sentotp.php

--- assets/allajaxphp/user/user_reg/sentotp.php
+++ assets/allajaxphp/user/user_reg/sentotp.php
@@ -10,7 +10,7 @@ if($ri>0){
 }else{
     $otp=rand(1111,9999);
 	$_SESSION['OTP']=$otp;
-	$email="ayondip2001@gmail.com";
+	$email="***@gmail.com";
 	$to=$_POST['mail'];
 	$subject='OTP';
 	$message="Your OTP is: ".$otp;
