ac35b5e1ebb36c419e479536982218321a944d3f
Ayondip Jana <64276906+dazal-32@users.noreply.github.com>
2021-02-19 19:17:29 +0530

    Update connection.php

--- Admin/assets/require/connection.php
+++ Admin/assets/require/connection.php
@@ -1,16 +1,6 @@
 <?php
 session_start();
-$con= mysqli_connect('localhost','root','');
-mysqli_select_db($con,'nandi_studio');
-/*if(!isset($_SESSION['USER_ID']))
-{
-	$_SESSION['USER_ID']='0';
-	}*/
-/*if($con)
-{
-echo"conected";
-}
-else{
-echo"not conected";
-}*/
+$con= mysqli_connect('host','host_username','host_password');
+mysqli_select_db($con,'db_name');
+
 ?>
